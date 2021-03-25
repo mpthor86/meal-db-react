7dd416859f296a77ac7bda6548ac1efcc65bbad0
michael <mpthornton86@gmail.com>
2021-03-25 08:57:34 -0600

    initial backend

---
+++ server/db/migrate/20210325145408_create_meals.rb
@@ -0,0 +1,14 @@
+class CreateMeals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meals do |t|
+      t.string :name
+      t.string :picture
+      t.string :area
+      t.string :category
+      t.string :video
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
