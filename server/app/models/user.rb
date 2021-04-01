65daa2a16b7ec89b03ef594fec599d3c7b2871e7
michael <mpthornton86@gmail.com>
2021-04-01 17:08:00 -0600

    fix has_many update DB

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many: meals
+    has_many :meals
+
+    validates :username, uniqueness: true, presence: true 
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_145408) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "has_secure_password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
