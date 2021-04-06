85fbaf44507d8d694196175a808a8f4cefde8ec0
michael <mpthornton86@gmail.com>
2021-04-06 15:32:26 -0600

    add links in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import CategoryContainer from './CategoryContainer'
 import '../Sidebar.css'
+import {Link} from 'react-router-dom'
 
 export default class Sidebar extends React.Component {
     render() {
         return(
             <div className="sidebar">
+                <Link to='/'>Home</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Signup</Link>
                 <CategoryContainer style={{fixed: "right"}}filterMeal={this.props.filterMeal} categories={this.props.categories}/>
             </div>
         )
