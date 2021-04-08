03087e8d796c85cc8f56094c946320d3a00af33c
michael <mpthornton86@gmail.com>
2021-04-08 14:56:24 -0600

    links for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,16 +2,33 @@ import React from 'react'
 import CategoryContainer from './CategoryContainer'
 import '../Sidebar.css'
 import {Link} from 'react-router-dom'
+import {logout} from '../actions/authActions'
+import {connect} from 'react-redux'
 
-export default class Sidebar extends React.Component {
+class Sidebar extends React.Component {
     render() {
         return(
             <div className="sidebar">
                 <Link to='/'>Home</Link>
+                {this.props.loggedIn ? (
+                    <>
+                    <Link to="/" onClick={() => this.props.logout()}>Logout</Link>
+                    </>
+                ) :
+                <>
                 <Link to='/login'>Login</Link>
                 <Link to='/signup'>Signup</Link>
-                <CategoryContainer style={{fixed: "right"}}filterMeal={this.props.filterMeal} categories={this.props.categories}/>
+                </>}
+                <CategoryContainer filterMeal={this.props.filterMeal} categories={this.props.categories}/>
             </div>
         )
     }
 }
+
+const stateToProps = state => {
+    return{
+        loggedIn: state.authReducer.loggedIn
+    }
+}
+
+export default connect(stateToProps, {logout})(Sidebar)
