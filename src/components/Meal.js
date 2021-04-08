15cb28b332c45c7c9ac1db874ffb5ce28d47382a
michael <mpthornton86@gmail.com>
2021-04-08 14:54:46 -0600

    like button

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,14 +1,13 @@
 import '../Meal.css'
 
 const Meal = (props) => {
-    console.log(props.meal)
     return(
         <div className="meal-card">
+            {props.loggedIn ? <button className='likeBtn'>Like</button> : ""}
             <h3>{props.meal.strMeal}</h3>
            Area: {props.meal.strArea}<br></br>
            Category: {props.meal.strCategory}<br></br>
-           <img className="meal-pic" alt="" src={props.meal.strMealThumb} />
-        </div>
+           <img className="meal-pic" alt="" src={props.meal.strMealThumb} />        </div>
     )
 }
 
