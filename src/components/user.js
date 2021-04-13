3bda2e54d6dd3d8fffb70c1a919669d0c096a504
michael <mpthornton86@gmail.com>
2021-04-13 13:39:35 -0600

    user meals index for profile

--- src/components/user.js
+++ src/components/user.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchUserMeals, deleteMeal, createMeal} from '../actions/userActions'
+import {getMealDetails} from '../actions/mealActions'
 import Meal from './Meal'
-import {fetchUserMeals} from '../actions/userActions'
 
 class User extends React.Component {
     componentDidMount(){
         this.props.fetchUserMeals(this.props.user)
     }
 
+    renderMeals(){
+        return this.props.meals.map((m) => <Meal loggedIn={this.props.loggedIn} handleClick={this.handleClick} key={m.idMeal} meal={m}/>)
+    }
+
+    handleClick = (e, meal) => {
+        if (e.target.innerText === 'Delete'){
+            this.props.deleteMeal(meal)
+        }else if(e.target.innerText === 'Details'){
+            this.props.getDetails(meal)
+        }else if(e.target.innerText === 'Like'){
+            this.props.createMeal(meal)
+        }
+    }  
+
     render(){
-        console.log(this.props.user)
         return(
             <div>
-                {this.props.user.username}'s profile<br></br>
-                {this.props.userMeals.map((m) => <Meal meal={m} />)}
+                <h3>{this.props.user.username}'s profile</h3>
+                <h3><u>Your Meals</u></h3>
+                {this.props.loading === true ? "Loading" : this.renderMeals()}
             </div>
         )
     }
@@ -22,13 +37,18 @@ class User extends React.Component {
 const stateToProps = state =>{
     return{
         user: state.authReducer.currentUser,
-        userMeals: state.mealReducer.userMeals
+        meals: state.mealReducer.userMeals,
+        loading: state.mealReducer.loading,
+        loggedIn: state.authReducer.loggedIn
     }
 }
 
 const dispToProps = disp => {
     return{
-        fetchUserMeals: (user) => disp(fetchUserMeals(user))
+        createMeal: (meal, user) => disp(createMeal(meal, user)),
+        getDetails: (mealId) => disp(getMealDetails(mealId)),
+        fetchUserMeals: (user) => disp(fetchUserMeals(user)),
+        deleteMeal: (meal) => disp(deleteMeal(meal))
     }
 }
 
