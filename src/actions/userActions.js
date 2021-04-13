9bec163c2e8f7b5eb24b1a64ddd75a68ef77efc8
michael <mpthornton86@gmail.com>
2021-04-13 13:38:23 -0600

    add delete

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -31,8 +31,24 @@ export const createMeal = (meal) => {
     }
 }
 
+export const deleteMeal = (meal) => {
+    return disp => {
+        fetch(`${url}/meals/${meal.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({meal: meal})
+        })
+        .then(resp => resp.json())
+        .then(data => disp({type: 'DELETE_USER_MEAL', payload: {message: data.message}}))
+    }
+}
+
 export const fetchUserMeals = (user) => {
     return disp => {
+        disp({type: 'GET_MEALS'})
         fetch(`${url}/meals`, {credentials: 'include'})
         .then(res => res.json())
         .then(json => disp({type: 'ADD_USER_MEALS', payload: {meals: json.meals}}))
