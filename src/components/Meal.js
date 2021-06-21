3a7b1a58730c0f4fc4d4dbf9aeadf4beb8517463
michael <mpthornton86@gmail.com>
2021-06-21 15:27:55 -0600

    Prevent overlap

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -3,16 +3,22 @@ import '../Meal.css'
 const Meal = (props) => {
     return(
         <div className="meal-card">
-            {props.meal.id ? <button className="likeBtn" onClick={(e) => props.handleClick(e, props.meal)}>Delete</button> : <button className="likeBtn" onClick={(e) => props.handleClick(e, props.meal)}>Like</button>}
-            {props.meal.strArea ? <h3>{props.meal.strMeal}</h3> : <h4>{props.meal.strMeal}<button onClick={(e) => props.handleClick(e, props.meal.idMeal)}>Details</button></h4>}
-           <img className="meal-pic" alt="" src={props.meal.strMealThumb} />        
-           {props.meal.strArea ? <div>
-               <p>Area: {props.meal.strArea}</p>
-               <p>Category: {props.meal.strCategory}</p>
-               {props.meal.strYoutube ? <p>Video:{props.meal.strYoutube}</p> : ""}
-               </div> : ""}
+            <h3>{props.meal.strMeal}</h3>
+            <img className="meal-pic" alt="" src={props.meal.strMealThumb} />
+            <button className="likeBtn" onClick={(e) => props.mealClick(e, props.meal)}>Details</button>
         </div>
     )
 }
 
 export default Meal
+
+//<div className="meal-card">
+//            {props.meal.id ? <button className="likeBtn" onClick={(e) => props.handleClick(e, props.meal)}>Delete</button> : <button className="likeBtn" onClick={(e) => props.handleClick(e, props.meal)}>Like</button>}
+//            {props.meal.strArea ? <h3>{props.meal.strMeal}</h3> : <h4>{props.meal.strMeal}<button onClick={(e) => props.handleClick(e, props.meal.idMeal)}>Details</button></h4>}
+//           <img className="meal-pic" alt="" src={props.meal.strMealThumb} />        
+//           {props.meal.strArea ? <div>
+//               <p>Area: {props.meal.strArea}</p>
+//               <p>Category: {props.meal.strCategory}</p>
+//               {props.meal.strYoutube ? <p>Video:{props.meal.strYoutube}</p> : ""}
+//               </div> : ""}
+//        </div>
