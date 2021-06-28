73f8ddcbaac11b2c0e49e2e95b8bb683cf522cc6
michael <mpthornton86@gmail.com>
2021-06-28 16:55:21 -0600

    Home page logo

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1><u>The Meal DB on React</u><img className="logo" src='/spaghetti logo.png' alt="not found"></img></h1>
+        <h1><img src='/wrangler.png' alt='not found'></img><img className="logo" src='/spaghetti logo.png' alt="not found"></img></h1>
           <Router>
                 <Sidebar className='sidebar'/>
             <Switch>
